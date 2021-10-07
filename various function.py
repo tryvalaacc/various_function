6ab9cf23e881830b2d199f5f1e6a2b17a38cce04
tryvalaacc <72210439+tryvalaacc@users.noreply.github.com>
2021-10-07 20:11:57 +0530

    Add files via upload

---
+++ various function.py
@@ -0,0 +1,21 @@
+# various built in function in python
+
+#len function
+mark=[45,54,95,62]
+print("lengh function",len(mark))
+
+#sum function
+print("sum function",sum(mark))
+
+# absolute function
+a=(3+3j)
+print("abdolute func",abs(a))
+
+#enumarte function
+mylist=["apple","banana","grapes"]
+print("enumarte func",list(enumerate(mylist))) #will show the index value of an item 
+
+#format function
+x=0.5
+print("formate func",format(x,"%"))
+
